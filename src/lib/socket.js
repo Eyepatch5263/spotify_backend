2f981a38e06e71a6dae6c150c40eba791a95c4c5
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-12-17 19:03:40 +0530

    final changes

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -4,7 +4,7 @@ const Messsage = require("../models/message.model")
 const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "https://spotify-eye-ic6bk.ondigitalocean.app/",
+            origin: "https://spotify-eye-ic6bk.ondigitalocean.app",
             credentials: true
         }
     })
